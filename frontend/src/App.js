6a8d3ed832c1b3b3f5941223368bd50f059715be
Huy Nguyen <JohnasHuy21091996@gmail.com>
2020-07-01 14:24:58 +0700

    Initial source

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { Button } from './components/basic';
+
+function App() {
+  return (
+    <div className="App">
+      <Button 
+        label={'Test'}
+      />
+    </div>
+  );
+}
+
+export default App;
